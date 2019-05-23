47348b112cfa6a535a9d11b1c0205574fe7ab576
hexueyuan <im.hexueyuan@outlook.com>
2019-05-23 15:17:01 +0800

    update:collector conf

--- collector/main.py
+++ collector/main.py
@@ -2,48 +2,55 @@
 
 from collector import Collector
 from database import SQLiteBase
-from dashboard import Logger, Config, Dashboard
 
 import json
 import sys
 import os
 import signal
 
-def main(conf):
-    ## 创建日志工厂
-    logFactory = Logger(conf['log'])
-    ## 创建默认数据库插件
-    db = SQLiteBase(conf['database']['path'], logger=logFactory.getLogger('database') ,init_script_file=conf['database'].get('init_script_file'))
-    ## 创建默认配置文件插件
-    config = conf['config']
+sys.path.append('../lib')
+from dashboard import Logger, Config, Dashboard, convertDashboardConf, convertLoggerConf
 
-    ## dashboard配置
-    dashboard_init = {
-        'namespace': {
-            'collector': {
-                'config': config.get('collector'),
-                'logger': logFactory.getLogger('collector'),
-                'database': None
-            },
-            'cpuWorker': {
-                'config': config.get('cpuWorker'),
-                'logger': logFactory.getLogger('cpuWorker'),
-                'database': db
-            },
-            'memoryWorker': {
-                'config': config.get('memoryWorker'),
-                'logger': logFactory.getLogger('memoryWorker'),
-                'database': db
-            },
-            'diskWorker': {
-                'config': config.get('diskWorker'),
-                'logger': logFactory.getLogger('diskWorker'),
-                'database': db
-            }
-        }
-    }
-    ## 创建dashboard
-    dashboard = Dashboard(dashboard_init)
+def init_dashboard(db, mod_list, loggerFactory, config, datapool, debug=False):
+    for mod in mod_list:
+        if debug:
+            db.setPlugin(mod, 'logger', loggerFactory.getLogger(mod + 'Debug'))
+        else:
+            db.setPlugin(mod, 'logger', loggerFactory.getLogger(mod))
+        db.setPlugin(mod, 'config', config.get(mod, {}))
+        db.setPlugin(mod, 'database', datapool)
+
+def create_dashboard(conf):
+    dashboardConf = convertDashboardConf(conf)
+    #print json.dumps(dashboardConf, indent=2)
+    # --> 创建默认配置文件插件
+    config = dashboardConf['config']
+    mainConf = config.get('main', {})
+
+    # 创建dashboard
+    # --> 创建日志工厂
+    loggerConf = convertLoggerConf(dashboardConf['logger'])
+    #print json.dumps(loggerConf, indent=2)
+    logFactory = Logger(loggerConf)
+    
+    # --> 创建默认数据库插件
+    dbConf = config.get('database')
+    if mainConf.get('debug'):
+        db = SQLiteBase(dbConf['path'], logger=logFactory.getLogger('databaseDebug') ,init_script_file=dbConf.get('init_script_file'))
+    else:
+        db = SQLiteBase(dbConf['path'], logger=logFactory.getLogger('database') ,init_script_file=dbConf.get('init_script_file'))
+    dashboard = Dashboard()
+
+    submodList = ['main', 'collector', 'cpuWorker', 'memoryWorker', 'diskWorker']
+    init_dashboard(dashboard, submodList, logFactory, config, db, debug=mainConf.get('debug', False))
+
+    return dashboard
+
+def main(conf):
+    dashboard = create_dashboard(conf)
+
+    logger = dashboard.getPlugin('main', 'logger')
+    logger.info("set dashboard.")
 
     ## 创建collector
     collector = Collector(dashboard)
@@ -51,13 +58,13 @@ def main(conf):
     ## 注册捕获信号
     def exitHandler(signum, frame):
         collector.exit()
-        logFactory.getLogger('root').info('collector exit.')
+        logger.info('collector exit.')
         exit()
     signal.signal(signal.SIGINT, exitHandler)
     signal.signal(signal.SIGTERM, exitHandler)
     signal.signal(signal.SIGTSTP, exitHandler)
 
-    logFactory.getLogger('root').info('collector start.')
+    logger.info('collector start.')
     ## 启动collector
     collector.work()
 
